039ba2778cc30d4e800eae8e620a0f797c28afb4
marionpol <marion.poljakov@gmail.com>
2024-01-30 15:36:23 +0200

    UL 5 Filtering

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -4,18 +4,19 @@ import { useState } from 'react';
 
 const ExpensesFilter = (props) => {
 
-    const [filteredYear, setFilteredYear] = useState('')
-    console.log(filteredYear)
+    const [filteredYear, setFilteredYear] = useState('');
 
     const filterYearHandler = (event) => {
-        setFilteredYear(event.target.value)
-    }
+        const selectedYear = event.target.value;
+        setFilteredYear(selectedYear);
+        props.onChangeFilter(selectedYear);
+    };
 
     return (
         <div className='expenses-filter'>
             <div className='expenses-filter__control'>
                 <label>Filter by year</label>
-                <select onChange={filterYearHandler}>
+                <select value={filteredYear} onChange={filterYearHandler}>
                     <option value='2023'>2023</option>
                     <option value='2024'>2024</option>
                     <option value='2025'>2025</option>
