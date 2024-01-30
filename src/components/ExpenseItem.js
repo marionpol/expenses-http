455cb05489c9a8bd9e208c62a91860f41a327d1f
marionpol <marion.poljakov@gmail.com>
2024-01-30 12:27:44 +0200

    Added a clickable button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from './UI/Card';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.expenseData.title)
 
+    const clickHandler = () => {
+        console.log('Clicked!')
+        setTitle(`Updated by click ${title}`)
+        console.log(title)
+    }
 
     return(<Card className="expense-item">
        <ExpenseDate
@@ -12,6 +19,7 @@ const ExpenseItem = (props) => {
             <h2>{props.expenseData.title}</h2>
             <div className='expense-item__price'>{props.expenseData.price}</div>
         </div>
+        <button onClick={clickHandler}>Click Me</button>
     </Card>)
 }
 
