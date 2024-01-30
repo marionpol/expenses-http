ac9c3bca860bb61b714f68e00e2284bd3d703a3d
marionpol <marion.poljakov@gmail.com>
2024-01-30 16:36:06 +0200

    Removed the click me button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,9 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
-import { useState } from 'react';
+
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.expenseData.title)
-
-    const clickHandler = () => {
-        console.log('Clicked!')
-        setTitle(`Updated by click ${title}`)
-        console.log(title)
-    }
 
     return(
     <li><Card className="expense-item">
@@ -20,7 +13,7 @@ const ExpenseItem = (props) => {
             <h2>{props.expenseData.title}</h2>
             <div className='expense-item__amount'>{props.expenseData.amount}</div>
         </div>
-        <button onClick={clickHandler}>Click Me</button>
+
     </Card></li>)
 }
 
