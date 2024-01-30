4e8a4dc8ee8a42104c716b69afc756293221a172
marionpol <marion.poljakov@gmail.com>
2024-01-30 12:09:51 +0200

    UL 3

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card';
 
 function ExpenseItem(props){
 
 
-    return(<div className="expense-item">  
+    return(<Card className="expense-item">
        <ExpenseDate
        date={props.expenseData.date}/>
         <div className='expense-item__description'>
             <h2>{props.expenseData.title}</h2>
             <div className='expense-item__price'>{props.expenseData.price}</div>
         </div>
-    </div>)
+    </Card>)
 }
 
 export default ExpenseItem;
